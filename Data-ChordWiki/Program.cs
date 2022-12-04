8e3d52c6467b615ab1fb369470ad270ac201c5de
cir9 <zjy445719111@qq.com>
2022-12-04 14:21:17 +0800

    Create data-fetching Project

---
+++ Data-ChordWiki/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_ChordWiki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
