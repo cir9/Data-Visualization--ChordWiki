afa7344510774a76787e549e3b907cbf6babb21e
cir9 <zjy445719111@qq.com>
2022-12-04 15:22:19 +0800

    read xmlNode

--- Data-ChordWiki/Program.cs
+++ Data-ChordWiki/Program.cs
@@ -19,18 +19,30 @@ namespace Data_ChordWiki
             Console.WriteLine();
 
             XmlDocument document = new XmlDocument();
-            document.Load(dataPath + @".\sitemap.xml");
+            document?.Load(dataPath + @".\sitemap.xml");
 
-            var root = document.DocumentElement;
-            Console.WriteLine("Pages to fetch: " + (root?.ChildNodes.Count ?? 0));
+            var root = document?.DocumentElement;
+            XmlNodeList? nodes = root?.ChildNodes;
+            int nodeCount = nodes?.Count ?? 0;
+
+            Console.WriteLine("Pages to fetch: " + nodeCount);
             Console.WriteLine();
 
-            Console.Write("Fetch pages from: #");
-            int count = int.Parse(Console.ReadLine() ?? "-1");
+            Console.Write("Fetch pages from: #0");
+            int startIndex = 0;
+            int.TryParse(Console.ReadLine(), out startIndex);
+            Console.WriteLine();
+
+            if (startIndex < 0 || startIndex > nodeCount || nodeCount == 0) return;
+
+            for (int i = startIndex; i < nodeCount; i++) {
+                string url = nodes?[i]?.FirstChild?.InnerText ?? "";
+
+                Console.WriteLine(url);
+                Console.ReadLine();
 
 
-
-
+            }
         }
     }
 }
